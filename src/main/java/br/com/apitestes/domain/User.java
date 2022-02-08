0e8cd7b17fa70238aefc168c8e2e3aabc9d18165
alkxyly <alkxyly@hotmail.com>
2022-02-07 22:03:11 -0300

    9. perfil de testes

--- src/main/java/br/com/apitestes/domain/User.java
+++ src/main/java/br/com/apitestes/domain/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,11 +15,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "tb_user")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String name;
 	
 	@Column(unique = true)
